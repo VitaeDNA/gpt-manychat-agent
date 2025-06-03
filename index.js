647efe1e5da9c5e7da2829e887e7e8ce9d2a3865
VitaeDNA <info@vitaedna.com>
2025-06-03 15:05:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ Stile: professionale, rassicurante, mai aggressivo.
   {
     model: "gpt-3.5-turbo",
     messages: gptMessages,  // Use the properly defined gptMessages array
-    temperature: 0.5
+    temperature: 0.7
   },
   {
     headers: {
